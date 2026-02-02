c12fe8fe14c9419cd59aae703d1cc3b70fc95162
Natalia Santiago <natalia.santiago1@icloud.com>
2026-02-02 14:08:55 -0500

    Updating Add Job Button

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,6 +1,14 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 export default function ProtectedRoute() {
+  const location = useLocation();
   const token = localStorage.getItem("token");
-  return token ? <Outlet /> : <Navigate to="/login" replace />;
+
+  // Not logged in -> send to login and remember where user tried to go
+  if (!token) {
+    return <Navigate to="/login" replace state={{ from: location }} />;
+  }
+
+  // Logged in -> render the nested route (dashboard, add-job, account, etc.)
+  return <Outlet />;
 }
